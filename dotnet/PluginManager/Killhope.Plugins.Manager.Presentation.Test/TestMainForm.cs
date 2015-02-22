e4e71d2a7b6029c7618539a54b7dbfa3a599a9c4
David-Allison <david.allison2@durham.ac.uk>
2015-02-22 09:48:34 +0000

    Added test for Form1 Bug
    Added Test Assembly
    Added InternalsVisibleTo
    Added test.

---
+++ dotnet/PluginManager/Killhope.Plugins.Manager.Presentation.Test/TestMainForm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Killhope.Plugins.Manager.Presentation;
+
+namespace Killhope.Plugins.Manager.Test
+{
+    [TestClass]
+    public class TestMainForm
+    {
+        [TestMethod, ExpectedException(typeof(ArgumentNullException))]
+        public void Form1FailsWithBothNull()
+        {
+            new Form1(null, null);
+        }
+    }
+}
